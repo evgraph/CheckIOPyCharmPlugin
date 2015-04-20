4bde1533bff1a736b68d044fe17c2d97a5215864
evgraph <vk@eblans.org>
2015-04-21 00:06:06 +0300

    Update CheckIOUser class structure

--- src/CheckIOUser.java
+++ src/CheckIOUser.java
@@ -14,23 +14,7 @@ public class CheckIOUser {
     return level;
   }
 
-  public void setLevel(int level) {
-    this.level = level;
-  }
-
-  public int getUid() {
-    return uid;
-  }
-
-  public void setUid(int uid) {
-    this.uid = uid;
-  }
-
   public String getUsername() {
     return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
   }
 }
