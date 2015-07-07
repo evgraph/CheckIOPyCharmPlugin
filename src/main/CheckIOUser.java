cb885044d65154eccda282caed13821d8fb122d1
evgraph <vk@eblans.org>
2015-07-07 20:08:18 +0300

    Add equals to user

--- src/main/CheckIOUser.java
+++ src/main/CheckIOUser.java
@@ -31,4 +31,24 @@ public class CheckIOUser {
   public void setUsername(String username) {
     this.username = username;
   }
+
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    CheckIOUser user = (CheckIOUser)o;
+
+    if (!username.equals(user.username)) return false;
+    if (uid != user.uid) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = username != null ? username.hashCode() : 0;
+    result = 31 * result + uid;
+    result = 31 * result + level;
+    return result;
+  }
 }
