d86ac65fed9c6c2abd12fe4687222c4e0087facf
evgraph <vk@eblans.org>
2015-07-09 15:50:11 +0300

    Add project icon

---
+++ src/ui/CheckIOIcons.java
@@ -0,0 +1,11 @@
+package ui;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class CheckIOIcons {
+  public static class ToolWindow {
+    public static final Icon TOOL_WINDOW_ICON = IconLoader.getIcon("/resources/newProjectIcon.png");
+  }
+}
