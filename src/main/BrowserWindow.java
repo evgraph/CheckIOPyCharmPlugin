01796399940ecbb8752908fbdd5777085b6e8c4f
evgraph <vk@eblans.org>
2015-07-14 18:48:23 +0300

    Merge BrowserPanel and BrowserWindow

--- src/main/BrowserWindow.java
+++ src/main/BrowserWindow.java
@@ -1,5 +1,7 @@
 package main;
 
+import com.intellij.ide.ui.LafManager;
+import com.intellij.ide.ui.laf.darcula.DarculaLookAndFeelInfo;
 import com.sun.istack.internal.NotNull;
 import javafx.application.Platform;
 import javafx.concurrent.Worker;
@@ -9,58 +11,134 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class BrowserWindow extends JFrame {
-  JFXPanel javafxPanel;
-  WebView webComponent;
-  JPanel mainPanel;
+  public static final String EVENT_TYPE_CLICK = "click";
+  public JFXPanel myPanel;
+  public WebView myWebComponent;
+  private StackPane myPane;
+  private WebEngine myEngine;
+  private ProgressBar myProgressBar;
+  private int width = 700;
+  private int height = 700;
+  private boolean showProgress = true;
+  private boolean refInNewBrowser = false;
 
-  public BrowserWindow() {
-
-    javafxPanel = new JFXPanel();
-
-    initSwingComponents();
+  public BrowserWindow(@NotNull final String url) {
+    init();
+    load(url);
   }
 
-  private void initSwingComponents() {
-    mainPanel = new JPanel();
-    mainPanel.setLayout(new BorderLayout());
-    mainPanel.add(javafxPanel, BorderLayout.CENTER);
+  public BrowserWindow(@NotNull final String url,
+                       final int width,
+                       final int height,
+                       final boolean showPeogress,
+                       final boolean refInNewBrowser) {
+    init();
+    this.width = width;
+    this.height = height;
+    this.showProgress = showPeogress;
+    this.refInNewBrowser = refInNewBrowser;
+    load(url);
+  }
 
-    this.add(javafxPanel);
+  private void init() {
+    myPanel = new JFXPanel();
+    initComponents();
+  }
+
+  public void updateLaf(boolean isDarcula) {
+    if (isDarcula) {
+      updateLafDarcula(myEngine);
+    }
+    else {
+      Platform.runLater(() -> {
+        myEngine.setUserStyleSheetLocation(null);
+        myEngine.reload();
+      });
+    }
+  }
+
+  private static void updateLafDarcula(@org.jetbrains.annotations.NotNull final WebEngine engine) {
+    Platform.runLater(() -> {
+      engine
+        .setUserStyleSheetLocation("file:///src/resources/myDarcula.css");
+      engine.reload();
+    });
+  }
+
+  private void initComponents() {
+    Platform.runLater(() -> {
+      myWebComponent = new WebView();
+      myPane = new StackPane();
+      myEngine = myWebComponent.getEngine();
+      if (showProgress) {
+        myProgressBar = makeProgressBarWithListener();
+        myPane.getChildren().addAll(myWebComponent, myProgressBar);
+      }
+      else {
+        myPane.getChildren().add(myWebComponent);
+      }
+      if (refInNewBrowser) {
+        initHyperlinkListener();
+      }
+      Scene scene = new Scene(myPane, width, height);
+      myPanel.setScene(scene);
+      myPanel.setVisible(true);
+      updateLaf(LafManager.getInstance().getCurrentLookAndFeel() instanceof DarculaLookAndFeelInfo);
+    });
+
+    this.add(myPanel);
     this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    this.setSize(700, 700);
+    this.setSize(width, height);
   }
 
 
   public void load(@NotNull final String url) {
-    Platform.runLater(() -> {
-      StackPane pane = new StackPane();
-      webComponent = new WebView();
-      webComponent.setVisible(false);
-      final WebEngine engine = webComponent.getEngine();
-      final ProgressBar progress = makeProgressBarWithListener(engine);
-      pane.getChildren().addAll(webComponent, progress);
+    Platform.runLater(() -> myEngine.load(url));
+  }
 
-      engine.load(url);
-      Scene scene = new Scene(pane, 700, 700);
-      javafxPanel.setScene(scene);
-      javafxPanel.setVisible(true);
+  private void initHyperlinkListener() {
+    myEngine.getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> {
+      if (newState == Worker.State.SUCCEEDED) {
+        final EventListener listener = ev -> {
+          String domEventType = ev.getType();
+          if (domEventType.equals(EVENT_TYPE_CLICK)) {
+            myEngine.setJavaScriptEnabled(true);
+            myEngine.getLoadWorker().cancel();
+
+            final String href = ((Element)ev.getTarget()).getAttribute("href");
+            final BrowserWindow browserWindow = new BrowserWindow(href);
+            browserWindow.setVisible(true);
+            ev.preventDefault();
+          }
+        };
+
+        final Document doc = myEngine.getDocument();
+        final NodeList nodeList = doc.getElementsByTagName("a");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+          ((EventTarget)nodeList.item(i)).addEventListener(EVENT_TYPE_CLICK, listener, false);
+        }
+      }
     });
   }
 
-  private ProgressBar makeProgressBarWithListener(@NotNull final WebEngine engine) {
+  private ProgressBar makeProgressBarWithListener() {
     final ProgressBar progress = new ProgressBar();
-    progress.progressProperty().bind(engine.getLoadWorker().progressProperty());
+    progress.progressProperty().bind(myEngine.getLoadWorker().progressProperty());
 
-    engine.getLoadWorker().stateProperty().addListener(
+    myEngine.getLoadWorker().stateProperty().addListener(
       (ov, oldState, newState) -> {
         if (newState == Worker.State.SUCCEEDED) {
-          progress.setVisible(false);
-          webComponent.setVisible(true);
+          myProgressBar.setVisible(false);
+          myWebComponent.setVisible(true);
         }
       });
     return progress;
--- src/ui/TaskInfoPanel.java
+++ src/ui/TaskInfoPanel.java
@@ -1,41 +1,24 @@
 package ui;
 
-import com.intellij.ide.ui.LafManager;
-import com.intellij.ide.ui.laf.darcula.DarculaLookAndFeelInfo;
-import com.intellij.openapi.application.ApplicationManager;
-import javafx.application.Platform;
-import javafx.concurrent.Worker;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import main.BrowserWindow;
-import org.jetbrains.annotations.NotNull;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.events.EventListener;
-import org.w3c.dom.events.EventTarget;
 
 import javax.swing.*;
 
 public class TaskInfoPanel extends JPanel {
-  public static final String EVENT_TYPE_CLICK = "click";
   private ButtonPanel myButtonPanel;
   private JLabel taskNameLabel;
-  private JFXPanel myTextPanel;
-  private BrowserPanel myBrowserPanel;
+  private static final int width = 450;
+  private static final int height = 1000;
+  private BrowserWindow myBrowserWindow;
 
 
   public TaskInfoPanel(String taskTextPath, String taskName) {
     setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
     myButtonPanel = new ButtonPanel();
     taskNameLabel = new JLabel(taskName);
-    myBrowserPanel = new BrowserPanel(taskTextPath);
-    myTextPanel = myBrowserPanel.myPanel;
+    myBrowserWindow = new BrowserWindow(taskTextPath, width, height, false, true);
     add(taskNameLabel);
-    add(myTextPanel);
+    add(myBrowserWindow.myPanel);
     add(myButtonPanel);
   }
 
@@ -49,93 +32,11 @@ public class TaskInfoPanel extends JPanel {
 
 
   public void setTaskText(String taskTextPath) {
-    myBrowserPanel.load(taskTextPath);
+    myBrowserWindow.load(taskTextPath);
   }
 
   public void updateLaf(boolean isDarcula) {
-    WebEngine engine = myBrowserPanel.webComponent.getEngine();
-
-    if (isDarcula) {
-      BrowserPanel.updateLafDarcula(engine);
-    }
-    else {
-      Platform.runLater(() -> {
-        engine.setUserStyleSheetLocation(null);
-        engine.reload();
-      });
-    }
-  }
-  private static class BrowserPanel {
-    public JFXPanel myPanel;
-    private WebView webComponent;
-    private BorderPane myBorderPane;
-    private Scene myScene;
-    private static final int width = 450;
-    private static final int height = 1000;
-
-    public BrowserPanel(@NotNull final String filePath) {
-      myPanel = new JFXPanel();
-      ApplicationManager.getApplication().invokeLater(() -> myPanel.setVisible(false));
-      initComponents();
-      load(filePath);
-    }
-
-    private void initComponents() {
-      Platform.runLater(() -> {
-        webComponent = new WebView();
-        myBorderPane = new BorderPane();
-        myBorderPane.setCenter(webComponent);
-        if (LafManager.getInstance().getCurrentLookAndFeel() instanceof DarculaLookAndFeelInfo) {
-          updateLafDarcula(webComponent.getEngine());
-        }
-        myScene = new Scene(myBorderPane, width, height);
-        myPanel.setScene(myScene);
-        initHyperlinkListener();
-      });
-    }
-
-    private void initHyperlinkListener() {
-      webComponent.getEngine().getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> {
-        if (newState == Worker.State.SCHEDULED) {
-          ApplicationManager.getApplication().invokeLater(() -> myPanel.setVisible(false));
-        }
-        if (newState == Worker.State.SUCCEEDED) {
-          ApplicationManager.getApplication().invokeLater(() -> myPanel.setVisible(true));
-
-          final EventListener listener = ev -> {
-            String domEventType = ev.getType();
-            if (domEventType.equals(EVENT_TYPE_CLICK)) {
-              webComponent.getEngine().setJavaScriptEnabled(true);
-              webComponent.getEngine().getLoadWorker().cancel();
-
-              final String href = ((Element)ev.getTarget()).getAttribute("href");
-              final BrowserWindow browserWindow = new BrowserWindow();
-              browserWindow.load(href);
-              browserWindow.setVisible(true);
-              ev.preventDefault();
-            }
-          };
-
-          final Document doc = webComponent.getEngine().getDocument();
-          final NodeList nodeList = doc.getElementsByTagName("");
-          for (int i = 0; i < nodeList.getLength(); i++) {
-            ((EventTarget)nodeList.item(i)).addEventListener(EVENT_TYPE_CLICK, listener, false);
-          }
-        }
-      });
-    }
-
-    private void load(@NotNull final String filePath) {
-      Platform.runLater(() -> webComponent.getEngine().load(filePath));
-    }
-
-    private static void updateLafDarcula(@NotNull final WebEngine engine) {
-      Platform.runLater(() -> {
-        engine
-          .setUserStyleSheetLocation("file:///home/user/work/CheckIO/CheckIOPyCharmPlugin/src/resources/myDarcula.css");
-        engine.reload();
-      });
-    }
+    myBrowserWindow.updateLaf(isDarcula);
   }
 
   public void setTaskNameLabelText(String taskName) {
