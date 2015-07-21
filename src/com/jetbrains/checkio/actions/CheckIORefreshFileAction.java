8c677508c4257d6f0b7c079a578fa40a2143b3fc
evgraph <vk@eblans.org>
2015-07-21 14:23:33 +0300

    Add pop-up with refresh operation result

--- src/com/jetbrains/checkio/actions/CheckIORefreshFileAction.java
+++ src/com/jetbrains/checkio/actions/CheckIORefreshFileAction.java
@@ -1,21 +1,21 @@
 package com.jetbrains.checkio.actions;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.ide.projectView.ProjectView;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.KeyboardShortcut;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.impl.DocumentImpl;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.keymap.KeymapUtil;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
 import com.jetbrains.checkio.CheckIOTaskManager;
 import com.jetbrains.checkio.CheckIOUtils;
 import com.jetbrains.edu.courseFormat.Task;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
@@ -38,25 +38,21 @@ public class CheckIORefreshFileAction extends DumbAwareAction {
       final Editor fileEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
       final Task task = CheckIOUtils.getTaskFromSelectedEditor(project);
       if (task == null || fileEditor == null) {
+        CheckIOUtils
+          .showOperationResultPopUp("Refresh action was called outside the editor", MessageType.ERROR.getPopupBackground(), project);
         LOG.warn("Refresh action was called outside the editor");
         return;
       }
       final CheckIOTaskManager taskManager = CheckIOTaskManager.getInstance(project);
       final String taskText = taskManager.myInitialTaskTextMap.get(task.getName());
 
-      final Document document = fileEditor.getDocument();
-      clearDocument(document);
+      final DocumentImpl document = (DocumentImpl)fileEditor.getDocument();
       ApplicationManager.getApplication().runWriteAction(() -> {
         document.setText(taskText);
+        CheckIOUtils.showOperationResultPopUp("Task refreshed", MessageType.INFO.getPopupBackground(), project);
+        ProjectView.getInstance(project).refresh();
       });
     }
 
-  }
-
-  private static void clearDocument(@NotNull final Document document) {
-    final int lineCount = document.getLineCount();
-    if (lineCount != 0) {
-      CommandProcessor.getInstance().runUndoTransparentAction(() -> document.deleteString(0, document.getLineEndOffset(lineCount - 1)));
-    }
   }
 }
