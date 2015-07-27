fc0cf119af775f91fed89851f9bcb52f8828a9a1
evgraph <vk@eblans.org>
2015-07-27 20:07:54 +0300

    Remove show solutions button from task info panel

--- src/com/jetbrains/checkio/ui/CheckIOTaskInfoPanel.java
+++ src/com/jetbrains/checkio/ui/CheckIOTaskInfoPanel.java
@@ -8,7 +8,6 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 
 public class CheckIOTaskInfoPanel extends JPanel {
-  private final ButtonPanel myButtonPanel;
   private final JLabel taskNameLabel;
   private final CheckIOBrowserWindow myBrowserWindow;
 
@@ -17,16 +16,10 @@ public class CheckIOTaskInfoPanel extends JPanel {
     final String taskTextPath = CheckIOUtils.getTaskTextUrl(project, task);
 
     setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-    myButtonPanel = new ButtonPanel();
     taskNameLabel = new JLabel(task.getName());
     myBrowserWindow = new CheckIOBrowserWindow(taskTextPath, CheckIOUtils.width, CheckIOUtils.height, false, true);
     add(taskNameLabel);
     add(myBrowserWindow.myPanel);
-    add(myButtonPanel);
-  }
-
-  public JButton getShowSolutionsButton() {
-    return myButtonPanel.myShowSolutionButton;
   }
 
   public void setTaskText(String taskTextPath) {
@@ -35,20 +28,5 @@ public class CheckIOTaskInfoPanel extends JPanel {
 
   public void setTaskNameLabelText(String taskName) {
     this.taskNameLabel.setText(taskName);
-  }
-
-  private static class ButtonPanel extends JPanel {
-    private static final String SHOW_SOLUTIONS_BUTTON_TEXT = "Show solutions";
-    private JButton myShowSolutionButton;
-
-    public ButtonPanel() {
-      initButtons();
-      add(myShowSolutionButton);
-    }
-
-    private void initButtons() {
-      myShowSolutionButton = new JButton(SHOW_SOLUTIONS_BUTTON_TEXT);
-      myShowSolutionButton.setEnabled(false);
-    }
   }
 }
