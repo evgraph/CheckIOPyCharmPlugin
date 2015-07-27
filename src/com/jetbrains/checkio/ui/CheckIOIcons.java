1b2af1829532d1d68aabad7b5d534c92cf0158ec
evgraph <vk@eblans.org>
2015-07-27 17:18:58 +0300

    Add icons for solutions tree structure

--- src/com/jetbrains/checkio/ui/CheckIOIcons.java
+++ src/com/jetbrains/checkio/ui/CheckIOIcons.java
@@ -8,4 +8,7 @@ public class CheckIOIcons {
   public static final Icon NEW_PROJECT = IconLoader.getIcon("/icons.checkio/newProjectIcon.png", CheckIOIcons.class);
   public static final Icon AUTHORIZATION = IconLoader.getIcon("/icons.checkio/authorizationButton.png", CheckIOIcons.class);
   public static final Icon SHOW_SOLUTIONS = IconLoader.getIcon("/icons.checkio/solutions.png", CheckIOIcons.class);
+  public static final Icon PYTHON_2 = IconLoader.getIcon("/icons.checkio/python2.png", CheckIOIcons.class);
+  public static final Icon PYTHON_3 = IconLoader.getIcon("/icons.checkio/python3.png", CheckIOIcons.class);
+
 }
