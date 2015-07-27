1b678d151776eeaf1c26d1eef620d79a16e795cf
evgraph <vk@eblans.org>
2015-07-27 18:44:05 +0300

    Enable show solutions action only for solved tasks

--- src/com/jetbrains/checkio/actions/CheckIOShowSolutionsAction.java
+++ src/com/jetbrains/checkio/actions/CheckIOShowSolutionsAction.java
@@ -3,6 +3,7 @@ package com.jetbrains.checkio.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.KeyboardShortcut;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.keymap.KeymapUtil;
@@ -15,6 +16,8 @@ import com.jetbrains.checkio.ui.CheckIOSolutionsPanel;
 import com.jetbrains.checkio.ui.CheckIOTaskToolWindowFactory;
 import com.jetbrains.checkio.ui.CheckIOToolWindow;
 import com.jetbrains.edu.courseFormat.Task;
+import com.jetbrains.edu.learning.StudyTaskManager;
+import com.jetbrains.edu.learning.courseFormat.StudyStatus;
 import org.jetbrains.jps.service.SharedThreadPool;
 
 import javax.swing.*;
@@ -24,13 +27,13 @@ public class CheckIOShowSolutionsAction extends AnAction {
   public static final String ACTION_ID = "CheckIOShowSolutionsAction";
   private static final Logger LOG = Logger.getInstance(CheckIOShowSolutionsAction.class);
   public static final String SHORTCUT = "ctrl shift pressed H";
-
   public CheckIOShowSolutionsAction() {
     super("Show solutions (" + KeymapUtil.getShortcutText(new KeyboardShortcut(KeyStroke.getKeyStroke(SHORTCUT), null)) + ")",
           "Show solutions",
           CheckIOIcons.SHOW_SOLUTIONS);
   }
 
+  @Override
   public void actionPerformed(AnActionEvent e) {
     final Project project = e.getProject();
     if (project == null) {
@@ -55,5 +58,20 @@ public class CheckIOShowSolutionsAction extends AnAction {
         });
       }
     });
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    final Presentation presentation = e.getPresentation();
+    final Project project = e.getProject();
+    if (project != null) {
+      final Task task = CheckIOUtils.getTaskFromSelectedEditor(project);
+      if (task != null) {
+        StudyStatus status = StudyTaskManager.getInstance(project).getStatus(task);
+        presentation.setEnabled(status == StudyStatus.Solved);
+        return;
+      }
+    }
+    presentation.setEnabled(false);
   }
 }
